e1698eec6569eda7ade08699693c15f21a32a984
VuVietNghia <vvn0068@gmail.com>
2025-12-29 17:29:56 +0700

    bo comment

--- Week3/Day5(26-12)/ProblemSet9/Car/CarManager.py
+++ Week3/Day5(26-12)/ProblemSet9/Car/CarManager.py
@@ -10,17 +10,6 @@ class CarManager:
             print(f"ID: {car.id_car}, Team: {car.team}, Engine Supply: {car.engine_supply}, Driver: {car.driver}, Car Number: {car.car_number}")
 
     def add_car(self, team: str, engine_supply: str, driver: str, car_number: str):
-        """Add a new car to the system.
-        
-        Args:
-            team: Team name
-            engine_supply: Engine supplier name
-            driver: Driver name
-            car_number: Car racing number
-            
-        Returns:
-            Car: The newly created Car object
-        """
         id_car = len(self.cars) + 1
         new_car = Car(id_car, team, engine_supply, driver, car_number)
         self.cars.append(new_car)
@@ -28,18 +17,6 @@ class CarManager:
 
     def update_car(self, car_number: str, team: str = None, engine_supply: str = None, 
                    driver: str = None, new_car_number: str = None):
-        """Update an existing car's information.
-        
-        Args:
-            car_number: Car number to find and update
-            team: New team name (None to skip)
-            engine_supply: New engine supplier (None to skip)
-            driver: New driver name (None to skip)
-            new_car_number: New car number (None to skip)
-            
-        Returns:
-            bool: True if car was found and updated, False otherwise
-        """
         for car in self.cars:
             if car_number == car.car_number:
                 if team:
@@ -54,14 +31,6 @@ class CarManager:
         return False
 
     def remove_car(self, car_number: str):
-        """Remove a car from the system.
-        
-        Args:
-            car_number: Car number to remove
-            
-        Returns:
-            bool: True if car was found and removed, False otherwise
-        """
         for car in self.cars:
             if car_number == car.car_number:
                 self.cars.remove(car)
@@ -69,14 +38,6 @@ class CarManager:
         return False
     
     def find_car(self, car_number: str):
-        """Find a car by its car number.
-        
-        Args:
-            car_number: Car number to search for
-            
-        Returns:
-            Car: The Car object if found, None otherwise
-        """
         for car in self.cars:
             if car_number == car.car_number:
                 return car
